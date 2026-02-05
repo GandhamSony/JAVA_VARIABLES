6b75bb8dd3b0aebc9ca0359ab9a80b64e02469a7
GandhamSony <23951A12D9@iare.ac.in>
2026-02-05 20:06:04 +0530

    Add files via upload

---
+++ Two_Sum.java
@@ -0,0 +1,17 @@
+public class Two_Sum {
+    public static void main(String[]args){
+        int a = 100;
+        int b = 200;
+
+        System.out.println("Sum of Two numbers:"+ (a+b));
+
+        /*
+        int num1 = 100; //first number 
+        int num2 = 200; //second number
+
+        int num= num1+num2;
+        
+        System.out.println("Sum:"+num); 
+         */
+    }
+}
